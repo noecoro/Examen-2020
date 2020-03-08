4b6b14e5d7ad0dd5f7d83db0df7f9d77a5abd790
noecoro <52795935+noecoro@users.noreply.github.com>
2020-03-07 21:55:29 -0300

    Add files via upload

---
+++ 2020IngresoFuentes(febrero)/tres.js
@@ -0,0 +1,12 @@
+function mostrar() {
+}
+/*En el ingreso a un viaje en crucero nos 
+solicitan nombre ,
+ edad(mayores de 18),
+  sexo("f" o "m") y 
+  estado civil("soltero", "casado" o "viudo");
+a) El nombre del hombre casado más joven.
+b) El sexo y nombre del pasajero/a más viejo.
+c) La cantidad de mujeres que hay casadas o viudas.
+d) El promedio de edad entre las mujeres.
+e) El promedio de edad entre los hombres solteros. */
---
+++ 2020IngresoFuentes(febrero)/uno.js
@@ -0,0 +1,15 @@
+
+function mostrar() {
+
+}
+/* 
+Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+el tipo (validar "barbijo" , "jabón" o "alcohol") ,
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+la Marca y el fabricante.
+Se debe Informar al usuario lo siguiente:
+a) Del más caro de los Barbijos, la cantidad de unidades y el fabricante
+b) Del ítem con más unidades, el fabricante
+c) Cuántas unidades de jabones hay en total*/
